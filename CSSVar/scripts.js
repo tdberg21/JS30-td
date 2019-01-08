47052c991f4f1579006ba7e39f3dc295dc01225b
Tory Dannenberg <rfdberg@gmail.com>
2019-01-07 19:30:01 -0700

    Change images in all projects to scuba, change audio files in drumkit and add extra styles to customize in CSSVar

--- CSSVar/scripts.js
+++ CSSVar/scripts.js
@@ -1,13 +1,11 @@
 const inputs = document.querySelectorAll('input');
-console.log(inputs);
 
 function handleUpdate() {
   const suffix = this.dataset.sizing || '';
-  console.log(suffix)
   document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
 }
 
 inputs.forEach(input => {
   input.addEventListener('change', handleUpdate)
-  // input.addEventListener('mousemove', handleUpdate)
 })
+
--- Clock/scripts.js
+++ Clock/scripts.js
@@ -1,22 +1,30 @@
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
-
+const digHour = document.querySelector('.hour')
+const digMinute = document.querySelector('.minute')
+const digSecond = document.querySelector('.second')
+console.log(digHour)
 
 function setDate() {
   const now = new Date();
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
+  digSecond.innerHTML = seconds;
+
   
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const minutes = now.getMinutes();
   const minuteDegrees = ((minutes / 60) * 360) + 90;
+  digMinute.innerHTML = minutes;
+
 
   minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
   
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + 90;
+  digHour.innerHTML = hour;
 
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
